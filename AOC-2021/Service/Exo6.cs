f3251e46b1b2070b36016c786f237414ab28fead
antoine.gamain <antoine.gamain@orange.com>
2021-12-08 13:34:15 +0100

    sumary exo6

--- AOC-2021/Service/Exo6.cs
+++ AOC-2021/Service/Exo6.cs
@@ -16,15 +16,23 @@ namespace AOC_EXO7.Service
             GrpFish = new List<long>() { 3, 4, 3, 1, 2, 1, 5, 1, 1, 1, 1, 4, 1, 2, 1, 1, 2, 1, 1, 1, 3, 4, 4, 4, 1, 3, 2, 1, 3, 4, 1, 1, 3, 4, 2, 5, 5, 3, 3, 3, 5, 1, 4, 1, 2, 3, 1, 1, 1, 4, 1, 4, 1, 5, 3, 3, 1, 4, 1, 5, 1, 2, 2, 1, 1, 5, 5, 2, 5, 1, 1, 1, 1, 3, 1, 4, 1, 1, 1, 4, 1, 1, 1, 5, 2, 3, 5, 3, 4, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 5, 5, 1, 3, 3, 1, 2, 1, 3, 1, 5, 1, 1, 4, 1, 1, 2, 4, 1, 5, 1, 1, 3, 3, 3, 4, 2, 4, 1, 1, 5, 1, 1, 1, 1, 4, 4, 1, 1, 1, 3, 1, 1, 2, 1, 3, 1, 1, 1, 1, 5, 3, 3, 2, 2, 1, 4, 3, 3, 2, 1, 3, 3, 1, 2, 5, 1, 3, 5, 2, 2, 1, 1, 1, 1, 5, 1, 2, 1, 1, 3, 5, 4, 2, 3, 1, 1, 1, 4, 1, 3, 2, 1, 5, 4, 5, 1, 4, 5, 1, 3, 3, 5, 1, 2, 1, 1, 3, 3, 1, 5, 3, 1, 1, 1, 3, 2, 5, 5, 1, 1, 4, 2, 1, 2, 1, 1, 5, 5, 1, 4, 1, 1, 3, 1, 5, 2, 5, 3, 1, 5, 2, 2, 1, 1, 5, 1, 5, 1, 2, 1, 3, 1, 1, 1, 2, 3, 2, 1, 4, 1, 1, 1, 1, 5, 4, 1, 4, 5, 1, 4, 3, 4, 1, 1, 1, 1, 2, 5, 4, 1, 1, 3, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 4 };
         }
 
+        /// <summary>
+        /// Resout le probleme
+        /// </summary>
         public void Resolve()
         {
             int nbJour = 256;
             List<DimensionPoisson> dimensionPoissons = GetDimensionPoissonsFromInput(this.GrpFish);
-            dimensionPoissons = EvolDimensionPoissons(dimensionPoissons, nbJour);
+            dimensionPoissons = EvolDimensionPoissonsByDay(dimensionPoissons, nbJour);
             long nbPoisson = dimensionPoissons.Select(x => x.NbPoisson).Sum();
             Console.WriteLine($"Apres {nbJour} jours il y a {nbPoisson} poissons new algo");
         }
 
+        /// <summary>
+        /// Parse l'input et génère les dimensions initial
+        /// </summary>
+        /// <param name="grpFish"></param>
+        /// <returns></returns>
         private List<DimensionPoisson> GetDimensionPoissonsFromInput(List<long> grpFish)
         {
             List<DimensionPoisson> dimensionPoissons = InitDimensionPoisson();
@@ -35,7 +43,13 @@ namespace AOC_EXO7.Service
             return dimensionPoissons;
         }
 
-        private List<DimensionPoisson> EvolDimensionPoissons(List<DimensionPoisson> dimensionPoissons, int nbJour)
+        /// <summary>
+        /// Effectue X cycle et retourne la dimension calculé
+        /// </summary>
+        /// <param name="dimensionPoissons"></param>
+        /// <param name="nbJour"></param>
+        /// <returns></returns>
+        private List<DimensionPoisson> EvolDimensionPoissonsByDay(List<DimensionPoisson> dimensionPoissons, int nbJour)
         {
             for (int i = 1; i <= nbJour; i++)
             {
@@ -44,6 +58,11 @@ namespace AOC_EXO7.Service
             return dimensionPoissons;
         }
 
+        /// <summary>
+        /// Effectue un cycle et retourne les dimmensions calculer
+        /// </summary>
+        /// <param name="dimensionPoissons"></param>
+        /// <returns></returns>
         private List<DimensionPoisson> EvolDimensionPoissons(List<DimensionPoisson> dimensionPoissons)
         {
             List<DimensionPoisson> dimensionPoissonsFutur = InitDimensionPoisson();
@@ -92,6 +111,10 @@ namespace AOC_EXO7.Service
             return dimensionPoissonsFutur;
         }
 
+        /// <summary>
+        /// Initialise les différentes dimensions des poisson , 1 dim par jour avant la duplication
+        /// </summary>
+        /// <returns></returns>
         private List<DimensionPoisson> InitDimensionPoisson()
         {
             List<DimensionPoisson> dimensionPoissons = new List<DimensionPoisson>();
